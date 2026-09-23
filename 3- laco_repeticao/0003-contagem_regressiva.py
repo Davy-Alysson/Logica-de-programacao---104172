531d15496bde607a0b2b8557c46f2d35a6031df8
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:59:36 -0300

    Atividade contagem

---
+++ 3- laco_repeticao/0003-contagem_regressiva.py
@@ -0,0 +1,17 @@
+import os
+import time
+os.system("cls")
+
+numeroi = int(input("Digite um número inicial:"))
+numerof = int(input("Numero final:"))
+if numeroi > numerof:
+    for i in range(numeroi,numerof + 1,-1):
+        print(i)
+        time.sleep(1)
+elif numeroi < numerof:
+    for i in range(numeroi,numerof + 1,1):
+        print(i)
+        time.sleep(1)
+
+     
+
